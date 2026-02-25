6e39ff2e79320841cff0c32c85c6084129fe2ea9
Jay Z. <jidong.1272@gmail.com>
2026-02-25 18:32:37 -0500

    added fonts

--- Chimes/Core/DesignSystem/DesignConstants.swift
+++ Chimes/Core/DesignSystem/DesignConstants.swift
@@ -10,9 +10,15 @@ import SwiftUI
 struct DesignConstants {
     
     enum Fonts {
+        static let H1 = Font.custom("Montserrat-SemiBold", size: 36)
+        static let H2SemiBold = Font.custom("Montserrat-SemiBold", size: 18)
+        static let H2Regular = Font.custom("Montserrat-Regular", size: 18)
         
+        static let H3SemiBold = Font.custom("Montserrat-SemiBold", size: 16)
+        static let H3Regular = Font.custom("Montserrat-Regular", size: 16)
         
-        
-        
+        static let P = Font.custom("Inter", size: 15)
+        static let Button1 = Font.custom("Inter", size: 14)
+        static let Button2 = Font.custom("Inter", size: 12)
     }
 }
